e6331786feaca37572c3ade004fc1a0fd53be9aa
Raunaq Bhirangi <raunaqbhirangi@fb.com>
2022-06-08 11:16:10 -0400

    minor changes for packaging

--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@ import os
 import sys
 from setuptools import setup, find_packages
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name='reskin_sensor',
-    version='1.0.0',
-    author='Raunaq Bhirangi',
+    name="reskin_sensor",
+    version="1.0.0",
+    author="Raunaq Bhirangi",
     author_email="rbhirang@andrew.cmu.edu",
-    description='Data acquisition library for a ReSkin sensor',
+    description="Data acquisition library for a ReSkin sensor",
     long_description=read('README.md'),
     packages=find_packages(),
+    install_requires=["numpy>=1.21.3", "pyserial>=3.5"],
+    python_requires=">=3.6",
     url="https://github.com/raunaqbhirangi/reskin_sensor.git",
 )
