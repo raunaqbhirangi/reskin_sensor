c049048f8b1213f0fb6f1dc55df055e8094acdac
Raunaq Bhirangi <raunaqbhirangi@fb.com>
2022-06-14 18:29:36 -0400

    reformat with black

--- reskin_sensor/sensor_proc.py
+++ reskin_sensor/sensor_proc.py
@@ -29,7 +29,7 @@ class ReSkinProcess(Process):
         the output
     chunk_size : int
         Quantum of data piped from buffer at one time.
-    
+
     Methods
     -------
     start_streaming():
@@ -46,14 +46,16 @@ class ReSkinProcess(Process):
         Return the recorded buffer
     """
 
-    def __init__(self,
-                 num_mags:int = 1,
-                 port: str = None,
-                 baudrate: int = 115200,
-                 burst_mode: bool = True,
-                 device_id: int = -1,
-                 temp_filtered: bool = False,
-                 chunk_size: int = 10000):
+    def __init__(
+        self,
+        num_mags: int = 1,
+        port: str = None,
+        baudrate: int = 115200,
+        burst_mode: bool = True,
+        device_id: int = -1,
+        temp_filtered: bool = False,
+        chunk_size: int = 10000,
+    ):
         """Initializes a ReSkinProcess object."""
         super(ReSkinProcess, self).__init__()
         self.num_mags = num_mags
@@ -66,7 +68,7 @@ class ReSkinProcess(Process):
         self._pipe_in, self._pipe_out = Pipe()
         self._sample_cnt = Value(ct.c_uint64)
         self._buffer_size = Value(ct.c_uint64)
-        
+
         self._last_time = Value(ct.c_double)
         self._last_delay = Value(ct.c_double)
         self._last_reading = Array(ct.c_float, self.num_mags * 4)
@@ -84,12 +86,13 @@ class ReSkinProcess(Process):
     @property
     def last_reading(self):
         return ReSkinData(
-            time = self._last_time.value,
+            time=self._last_time.value,
             acq_delay=self._last_delay.value,
-            data = self._last_reading[:],
-            dev_id=self.device_id)
+            data=self._last_reading[:],
+            dev_id=self.device_id,
+        )
         # return self._last_reading
-    
+
     @property
     def sample_cnt(self):
         return self._sample_cnt.value
@@ -98,9 +101,9 @@ class ReSkinProcess(Process):
         """Start streaming data from ReSkin sensor"""
         if not self._event_quit_request.is_set():
             self._event_is_streaming.set()
-            print('Started streaming')
+            print("Started streaming")
 
-    def start_buffering(self, overwrite:bool=False):
+    def start_buffering(self, overwrite: bool = False):
         """
         Start buffering ReSkin data. Call is ignored if already buffering
 
@@ -114,12 +117,12 @@ class ReSkinProcess(Process):
         if not self._event_is_buffering.is_set():
             if overwrite:
                 # Warn that buffer is about to be overwritten
-                print('Warning: Overwriting non-empty buffer')
+                print("Warning: Overwriting non-empty buffer")
                 self.get_buffer()
             self._event_is_buffering.set()
         else:
             # Warn that data is already buffering
-            print('Warning: Data is already buffering')
+            print("Warning: Data is already buffering")
 
     def pause_buffering(self):
         """Stop buffering ReSkin data"""
@@ -132,7 +135,7 @@ class ReSkinProcess(Process):
     def get_data(self, num_samples=5):
         """
         Return a specified number of samples from the ReSkin Sensor
-        
+
         Parameters
         ----------
         num_samples : int
@@ -141,40 +144,42 @@ class ReSkinProcess(Process):
         # Only sends samples if streaming is on. Sends empty list otherwise.
 
         samples = []
-        if num_samples <=0:
+        if num_samples <= 0:
             return samples
         last_cnt = self._sample_cnt.value
         samples = [self.last_reading]
         while len(samples) < num_samples:
             if not self._event_is_streaming.is_set():
-                print('Please start streaming first.')
+                print("Please start streaming first.")
                 return []
             # print(self._sample_cnt.value)
             if last_cnt == self._sample_cnt.value:
                 continue
             last_cnt = self._sample_cnt.value
             samples.append(self.last_reading)
-        
+
         return samples
 
-    def get_buffer(self, timeout:float=1.0, pause_if_buffering:bool=False):
+    def get_buffer(self, timeout: float = 1.0, pause_if_buffering: bool = False):
         """
         Return the recorded buffer
-        
+
         Parameters
         ----------
         timeout : int
             Time to wait for data to start getting piped.
-        
+
         pause_if_buffering : bool
             Pauses buffering if still running, and then collects and returns buffer
         """
         # Check if buffering is paused
         if self._event_is_buffering.is_set():
             if not pause_if_buffering:
-                print('Cannot get buffer while data is buffering. Set '
-                      'pause_if_buffering=True to pause buffering and '
-                      'retrieve buffer')
+                print(
+                    "Cannot get buffer while data is buffering. Set "
+                    "pause_if_buffering=True to pause buffering and "
+                    "retrieve buffer"
+                )
                 return
             else:
                 self._event_is_buffering.clear()
@@ -184,7 +189,7 @@ class ReSkinProcess(Process):
             while self._pipe_in.poll() or self._buffer_size.value > 0:
                 rtn.extend(self._pipe_in.recv())
             self._event_sending_data.clear()
-        
+
         return rtn
 
     def join(self, timeout=None):
@@ -194,7 +199,7 @@ class ReSkinProcess(Process):
         self.pause_streaming()
 
         super(ReSkinProcess, self).join(timeout)
-    
+
     def run(self):
         """This loop runs until it's asked to quit."""
         buffer = []
@@ -206,12 +211,13 @@ class ReSkinProcess(Process):
                 baudrate=self.baudrate,
                 burst_mode=self.burst_mode,
                 device_id=self.device_id,
-                temp_filtered=self.temp_filtered)
+                temp_filtered=self.temp_filtered,
+            )
             # self.sensor._initialize()
             self.start_streaming()
         except serial.serialutil.SerialException as e:
             self._event_quit_request.set()
-            print('ERROR: ', e)
+            print("ERROR: ", e)
             sys.exit(1)
 
         is_streaming = False
@@ -221,8 +227,11 @@ class ReSkinProcess(Process):
                     is_streaming = True
                     # Any logging or stuff you want to do when streaming has
                     # just started should go here
-                self._last_time.value, self._last_delay.value, \
-                    self._last_reading[:] = self.sensor.get_sample()
+                (
+                    self._last_time.value,
+                    self._last_delay.value,
+                    self._last_reading[:],
+                ) = self.sensor.get_sample()
 
                 self._sample_cnt.value += 1
 
@@ -232,13 +241,13 @@ class ReSkinProcess(Process):
                 elif self._buffer_size.value > 0:
                     self._event_sending_data.set()
                     chk = self._chunk_size
-                    while len(buffer)>0:
+                    while len(buffer) > 0:
                         if chk > len(buffer):
                             chk = len(buffer)
                         self._pipe_out.send(buffer[0:chk])
                         buffer[0:chk] = []
                         self._buffer_size.value = len(buffer)
-                    
+
             else:
                 if is_streaming:
                     is_streaming = False
@@ -247,11 +256,11 @@ class ReSkinProcess(Process):
                 if self._buffer_size.value > 0:
                     self._event_sending_data.set()
                     chk = self._chunk_size
-                    while len(buffer)>0:
+                    while len(buffer) > 0:
                         if chk > len(buffer):
                             chk = len(buffer)
                         self._pipe_out.send(buffer[0:chk])
                         buffer[0:chk] = []
                         self._buffer_size.value = len(buffer)
-        
+
         self.pause_streaming()
